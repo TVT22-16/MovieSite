fb06ca17b9d5221863bda64485e6c240bcbee564
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-11-14 12:40:54 +0200

    sometype of movie frontend

--- client/src/assets/Home.js
+++ client/src/assets/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import MovieApp from './MovieApp'
 
 function Home() {
   return (
     <div>
-        <h1>This is the homepage!
-        </h1>
+        <MovieApp/>
 
     </div>
   )
