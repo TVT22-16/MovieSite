4945e189d80c78c4780a411bcf50c6f833bd9d9e
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-11-14 14:53:41 +0200

    basic search working

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,34 +1,61 @@
-// MovieApp.js (Frontend)
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Movies.css'
-
+import './Movies.css';
 
 const Movies = () => {
   const [popularMovies, setPopularMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   const baseUrl = 'http://localhost:3001/movies';
 
   useEffect(() => {
-    
     // Fetch popular movies when the component mounts
-    axios.get(`${baseUrl}`) 
+    axios.get(baseUrl)
       .then(response => setPopularMovies(response.data))
       .catch(error => console.error('Error fetching popular movies:', error));
-  }, []);
+  }, [baseUrl]); // Add baseUrl as a dependency to useEffect
 
+  const handleSearch = () => {
+    if (searchTerm) {
+      axios.get(`${baseUrl}/${searchTerm}`) // Correct the search endpoint
+        .then(response => setSearchResults(response.data))
+        .catch(error => console.error('Error searching movies:', error));
+    }
+  };
 
   return (
+    <div className='outerCont'>
+      <h1>Movies</h1>
+            
+      <div className='searchCont'>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies..."
+        />
+        <button id='searchBtn' onClick={handleSearch}>Search</button>
+      </div>
 
-    <div class='outerCont'>
-      <h1>Popular Movies</h1>
-      <ul class='listCont'>
-        {popularMovies.map(movie => (
-          <li class='movieCard' key={movie.id}>
-            <h3 class='movieTitle'>{movie.title}</h3>
-            <img class='posterImg' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="Logo" />
-          </li>
-        ))}
+      <ul className='listCont'>
+        {searchResults.length > 0 ? (
+          // Render search results if available
+          searchResults.map(movie => (
+            <li className='movieCard' key={movie.id}>
+              <h3 className='movieTitle'>{movie.title}</h3>
+              <img className='posterImg' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="Movie Poster" />
+            </li>
+          ))
+        ) : (
+          // Render popular movies if no search results
+          popularMovies.map(movie => (
+            <li className='movieCard' key={movie.id}>
+              <h3 className='movieTitle'>{movie.title}</h3>
+              <img className='posterImg' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="Movie Poster" />
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
