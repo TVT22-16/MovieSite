986756f2be1f81952e830fd07969a0a8593db8ae
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-11-15 12:28:42 +0200

    basics of reviews ui

--- client/src/assets/Reviews.js
+++ client/src/assets/Reviews.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Reviews() {
-  return (
-    <div>
-        This is reviews
-    </div>
-  )
-}
+const baseURL =  'http://localhost:3001/reviews';
 
-export default Reviews
+export function Reviews() {
+    const [review, setReview] = React.useState(null);
+
+    React.useEffect(() => {
+      axios.get(baseURL).then((response) => {
+        setReview(response.data);
+      });
+    }, []);
+  
+    if (!review) return null;
+  
+    return (
+        <div id='body'>
+        <ul class='listCont'>
+                {review.map((rew, index) => (
+                <li key={index} class='listInReview'>
+                    <p>{(rew.username)}</p>
+                    <p>{(rew.review)}</p> 
+                    <p>{(rew.rating)}</p>
+
+                    {/* //Fetches movie by movieId from moviedb */}
+                    <p>{(()=>{
+                        
+                    })}</p>
+                </li>
+                ))}
+        </ul>
+        </div>
+    );
+  }
+
+
+//https://www.freecodecamp.org/news/how-to-use-axios-with-react/
+
+
+
+
+export default Reviews;
