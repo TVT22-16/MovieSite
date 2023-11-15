a517ccedded9cf74b7d0fc536e98f39594a4085f
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-11-15 13:03:45 +0200

    reviews fetches movie information by id

--- client/src/assets/Reviews.js
+++ client/src/assets/Reviews.js
@@ -1,43 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const baseURL =  'http://localhost:3001/reviews';
+const baseURL = 'http://localhost:3001/reviews';
+const moviebyidURL = 'http://localhost:3001/movies/id/';
 
 export function Reviews() {
-    const [review, setReview] = React.useState(null);
+  const [review, setReview] = React.useState(null);
+  const [movie, setMovie] = React.useState(null);
 
-    React.useEffect(() => {
-      axios.get(baseURL).then((response) => {
+  React.useEffect(() => {
+    const fetchReviewData = async () => {
+      try {
+        const response = await axios.get(baseURL);
         setReview(response.data);
-      });
-    }, []);
-  
-    if (!review) return null;
-  
-    return (
-        <div id='body'>
-        <ul class='listCont'>
-                {review.map((rew, index) => (
-                <li key={index} class='listInReview'>
-                    <p>{(rew.username)}</p>
-                    <p>{(rew.review)}</p> 
-                    <p>{(rew.rating)}</p>
+      } catch (error) {
+        console.error('Error fetching review data:', error);
+      }
+    };
 
-                    {/* //Fetches movie by movieId from moviedb */}
-                    <p>{(()=>{
-                        
-                    })}</p>
-                </li>
-                ))}
-        </ul>
-        </div>
-    );
-  }
+    fetchReviewData();
+  }, []);
 
+  React.useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        // Assuming movieid is available in the review data
+        const movieId = review?.[0]?.moviedb_movieid; // Adjust this based on your data structure
 
-//https://www.freecodecamp.org/news/how-to-use-axios-with-react/
+        if (movieId) {
+          const response = await axios.get(`${moviebyidURL}${movieId}`);
+          setMovie(response.data.title);
+        }
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+      }
+    };
 
+    fetchMovieData();
+  }, [review]); //runs if moviedata changes
 
+  if (!review || !movie) return null;
 
+  return (
+    <div id='body'>
+      <ul className='listCont'>
+        {review.map((rew, index) => (
+          <li key={index} className='listInReview'>
+            <p>{rew.username}</p>
+            <p>{rew.review}</p>
+            <p>{rew.rating}</p>
+
+            {/* Display movie title */}
+            <p>{movie}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Reviews;
