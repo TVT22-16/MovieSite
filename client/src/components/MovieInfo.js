4e3d924137247fbe2ea5a9676b8c3a6d0d816866
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-11-17 13:11:12 +0200

    movieInfo page information fetch works

--- client/src/components/MovieInfo.js
+++ client/src/components/MovieInfo.js
@@ -9,24 +9,24 @@ const MovieInfo = () => {
 
   const [searchParams] = useSearchParams();
   const [movieData, setMovieData] = useState({});
-  const [reviews, setReviews]=useState({})
+  const [reviews, setReviews]=useState([])
 
   const params = searchParams.get('id');
-
 
   useEffect(() => {
     axios.get(`http://localhost:3001/movies/id/${params}`)
     .then(res => {
-      const data = res.data;
-      setMovieData(data);
+      const response = res.data;
+      setMovieData(response);
     })
   },[params]); //prevent endless loop caused by useEffect and useState (only do this if params changes)
 
   useEffect(() => {
-    axios.get(`http://localhost:3001/movies/id/${params}`)
+    axios.get(`http://localhost:3001/reviews/byMovieId/${params}`)
     .then(res => {
-      const data = res.data;
-      setMovieData(data);
+      const response = res.data;
+      setReviews(response);
+      console.log(response);
     })
   },[params]); //prevent endless loop caused by useEffect and useState (only do this if params changes)
 
@@ -36,6 +36,15 @@ const MovieInfo = () => {
       <div>
         <h1>{movieData.title}</h1>
         <img className='infoPoster' src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`} alt="Movie Poster" />
+        <ul>
+          {reviews.map((review, index) => (
+            <li key={index}>
+              <strong> {review.username} </strong>
+              <strong> {review.review} </strong>
+              <strong> {review.rating} </strong>
+            </li>
+          ))}
+        </ul>
       </div>
 
   )
