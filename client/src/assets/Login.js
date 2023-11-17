11ab03b6c5ba35cd83a9213846742a326811e204
Eerik <112471297+Eerik-Vaisanen@users.noreply.github.com>
2023-11-17 09:31:49 +0200

    Updated login.js
    Samat animaatiot ja error messaget loginille kuin registerille

--- client/src/assets/Login.js
+++ client/src/assets/Login.js
@@ -8,7 +8,7 @@ function Login() {
 
   return (
     <div>
-      { jwtToken.value.length === 0 ? <LoginForm/> : 
+      {jwtToken.value.length === 0 ? <LoginForm /> :
         <button onClick={() => jwtToken.value = ''}>Logout</button>}
     </div>
   );
@@ -19,6 +19,10 @@ function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
@@ -31,18 +35,26 @@ function LoginForm() {
     e.preventDefault();
     console.log('Username: ' + username);
     console.log('Password ' + password);
+    setLoading(true);
+
+    if (!username.trim() || !password.trim()) {
+      setErrorMessage('All fields are required');
+      setLoading(false);
+      return;
+    }
+
     try {
       const response = await axios.post('http://localhost:3001/users/login', {
         username,
         password,
       })
-  
-    jwtToken.value = response.data.jwtToken;
-    usernameSignal.value = response.data.username;
 
-    //These may be needed later
-    sessionStorage.setItem('username', username);
-    sessionStorage.setItem('token', jwtToken.value);
+      jwtToken.value = response.data.jwtToken;
+      usernameSignal.value = response.data.username;
+
+      //These may be needed later
+      sessionStorage.setItem('username', username);
+      sessionStorage.setItem('token', jwtToken.value);
 
       console.log('Login successful:', response.data);
       setLoggedIn(true);
@@ -52,6 +64,14 @@ function LoginForm() {
     } catch (error) {
       console.error('Error during login:', error.response ? error.response.data : error.message);
       // Handle the error, e.g., display an error message to the user
+      if (error.response && error.response.status === 401) {
+        setErrorMessage(error.response.data.error);
+        setSuccessMessage('');
+      } else {
+        console.error('Error during login:', error.message);
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -98,9 +118,18 @@ function LoginForm() {
                 onChange={handlePasswordChange}
               />
             </div>
-            <button type="submit" className="btn btn-primary">
-              Login
+            <button type="submit" className="btn btn-primary" disabled={loading}>
+              {loading ? (
+                <>
+                  <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                  <span className="visually-hidden">Loading...</span>
+                </>
+              ) : (
+                'Login'
+              )}
             </button>
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
 
           </form>
 
