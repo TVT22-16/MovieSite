dae9f2b57ca32bdc722fdf7604968b00321c1a9a
unknown <joonasroska123@gmail.com>
2023-11-24 20:14:14 +0200

    genres and sorting ready

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Dropdown } from 'react-bootstrap';
 
 // Create the DropdownComponent
 const DropdownComponent = ({updateSort}) => {
 
+    const [sortTag,setSortTag] = useState('Popularity')
 
-    const handleSortChange = (sort) => {
+    const handleSortChange = (sort,tag) => {
 
         updateSort(sort);
+        setSortTag(tag);
     };
+
+
 
   return (
   <Dropdown>
     <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Sort By
+        {sortTag}
     </Dropdown.Toggle>
 
     <Dropdown.Menu>
 
-    <Dropdown.Item onClick={() => handleSortChange('popularity.desc')}>Popularity</Dropdown.Item>
+    <Dropdown.Item onClick={() => handleSortChange('popularity.desc','Popularity')}>Popularity</Dropdown.Item>
     {/* <Dropdown.Item onClick={() => handleSortChange('popularity.asc')}>Popularity (ascending)</Dropdown.Item> */}
 
-    <Dropdown.Item onClick={() => handleSortChange('vote_average.desc')}>Vote Average</Dropdown.Item>
+    <Dropdown.Item onClick={() => handleSortChange('vote_average.desc','Vote Average')}>Vote Average</Dropdown.Item>
     {/* <Dropdown.Item onClick={() => handleSortChange('vote_average.asc')}>Vote Average (ascending)</Dropdown.Item> */}
 
-    <Dropdown.Item onClick={() => handleSortChange('vote_count.desc')}>Vote Count</Dropdown.Item>
+    <Dropdown.Item onClick={() => handleSortChange('vote_count.desc','Vote Count')}>Vote Count</Dropdown.Item>
     {/* <Dropdown.Item onClick={() => handleSortChange('vote_count.asc')}>Vote Count (ascending)</Dropdown.Item> */}
 
-    <Dropdown.Item onClick={() => handleSortChange('revenue.desc')}>Revenue</Dropdown.Item>
+    <Dropdown.Item onClick={() => handleSortChange('revenue.desc','Revenue')}>Revenue</Dropdown.Item>
     {/* <Dropdown.Item onClick={() => handleSortChange('vote_average.asc')}>Vote Average (ascending)</Dropdown.Item> */}
 
     </Dropdown.Menu>
