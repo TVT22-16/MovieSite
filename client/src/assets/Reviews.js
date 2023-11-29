f17a8089df3affb623725e09826cdc7eb9abc713
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-11-29 13:57:18 +0200

    reviews filtering basics work

--- client/src/assets/Reviews.js
+++ client/src/assets/Reviews.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import BootstrapCard from '../components/BootstrapCardMovie';
+import { Card, CardFooter } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 
 const movieByIdURL = 'http://localhost:3001/movies/id';
 
 const Reviews = () => {
-  const [getReviewsUrl, setGetReviewsUrl] = useState('http://localhost:3001/reviews');
+  const [getReviewsUrl, setReviewsUrl] = useState('http://localhost:3001/reviews');
   const [reviews, setReviews] = useState([]);
   const [reviewsWData, setReviewsWData] = useState([]);
-  const [finalData, setFinalData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(getReviewsUrl);
         setReviews(response.data);
+        setReviewsWData([]);
       } catch (err) {
         console.error(err);
       }
@@ -41,32 +44,81 @@ const Reviews = () => {
     fetchMoviesData();
   }, [reviews]);
 
+  // const fetchUserReviews = async () => {
+  //   try {
+  //     const response = await axios.get('http://localhost:3001/reviews/user/Roope');
+  //     setReviews(response.data);
+  //     setReviewsWData([]); // Reset reviewsWData when fetching user-specific reviews
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
 
+  const updateGetReviewsUrl = (link) => {
+    setReviewsUrl(link)
+  }
 
-  // useEffect(() => {
-  //   // setFinalData(Array.prototype.push.apply(reviews, reviewsWData);
-  //   // Array.prototype.push.apply(reviews, reviewsWData)
-  //   // setFinalData(reviews.concat(reviewsWData));
-  //   setFinalData((finalData) => [...reviews, ...reviewsWData]);
+  // const chunkArray = (arr, size) => {
+  //   const chunkedArr = [];
+  //   for (let i = 0; i < arr.length; i += size) {
+  //     chunkedArr.push(arr.slice(i, i + size));
+  //   }
+  //   return chunkedArr;
+  // };
 
-  // }, [reviewsWData]);
+  // const reviewsChunks = chunkArray(reviewsWData, 4);
 
   return (
-    <div>
-    {(reviewsWData.length > 0 ? (<>{reviewsWData.map((fd,index) => (
+    <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+      {/* <button onClick={fetchUserReviews}>
+        Your reviews
+      </button> */}
+
+      <Dropdown>
+      <Dropdown.Toggle variant="primary" id="dropdown-basic">
+        Filter
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+
+      <Dropdown.Item onClick={() => updateGetReviewsUrl('http://localhost:3001/reviews')}>Reviews</Dropdown.Item>
+      {/* <Dropdown.Item onClick={() => handleSortChange('popularity.asc')}>Popularity (ascending)</Dropdown.Item> */}
+
+      <Dropdown.Item onClick={() => updateGetReviewsUrl('http://localhost:3001/reviews/user/Roope')}>Your reviews</Dropdown.Item>
+      {/* <Dropdown.Item onClick={() => handleSortChange('vote_average.asc')}>Vote Average (ascending)</Dropdown.Item> */}
+
+
+      </Dropdown.Menu>
+    </Dropdown>
+
+
+    {((reviewsWData.length > 0 && reviews.length > 0) ? (
         <>
-          <div>{fd.title}</div>
-          <div>{reviews[index].review}</div>
+          {reviewsWData.map((fd, index) => (
+            <div key={index} style={{    
+              display: 'grid',
+              gridTemplateColumns:'repeat(4, 1fr)'}}>
+
+              <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${fd.poster_path}`} />
+                <Card.Body>
+                  <Card.Title>{fd.title} - {reviews[index].rating}</Card.Title>
+
+                  <Card.Text>
+                    {reviews[index].review}
+                  </Card.Text>
+
+                  <CardFooter>{reviews[index].username}</CardFooter>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
         </>
-
-
-      ))}</>) : (
-      
-      <div> Loading reviews...</div>))
-    }
+      ) : (
+        <div>Loading reviews...</div>
+      ))}
     </div>
   );
 };
 
 export default Reviews;
-
