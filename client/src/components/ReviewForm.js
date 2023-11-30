07acad143c11399b83763276896f82f2be2eea1d
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-11-30 14:57:46 +0200

    review form has slider

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MovieInfo.css';
+import SliderComponent from './Slider';
 
 
 //Add review button and form for movieInfo page
@@ -41,28 +42,50 @@ const ReviewForm = ({moviedb_movieid}) => {
       });
     }
 
+
+
+
     const handleShowForm = () => {
-      setShowForm(true);
+      if (showForm === true){
+        setShowForm(false);
+      } else {
+        setShowForm(true);
+      }
     };
+
   
     return (
-      <div id="reviewFormBody" style={{width:'60%'}}>
-        <button className="addReviewBtn" onClick={handleShowForm}>
-          Add Review
-        </button>
+      <div id="reviewFormBody" style={{width:'100%', display:'flex', flexDirection:'column', alignItems:'center'}}>
+            <button type="submit" className="btn btn-primary" onClick={handleShowForm} style={{width:'20%'}}>
+              Review Movie
+            </button>
     
         {showForm && (
-          <form id="reviewForm" onSubmit={submitReviewForm}>
-            <textarea
+          <form id="reviewForm" onSubmit={submitReviewForm}
+          style={{
+            gap:'15px',display:'flex',flexDirection:'column', width:'50%', backgroundColor:'white', borderRadius:'5px', padding:'10px', opacity:'0.75'
+          }}>
+            {/* <textarea
               name="review"
               rows="4"
-              cols="50"
+              cols="100"
               placeholder="Write your review here"
               className="reviewBox"
+              
+              style={{width:'100%'}}
+            /> */}
+            <div style={{width:'100%'}}>
+            <textarea
+              id="review"
+              name="review"
+
+              className="input-xlarge"
+
+              style={{width:'100%'}}
             />
-    
-            <label htmlFor="rating">Rating</label>
-            <input
+            </div>
+
+            {/* <input
               type="number"
               name="rating"
               className="ratingBox"
@@ -70,9 +93,30 @@ const ReviewForm = ({moviedb_movieid}) => {
               step="any"
               min="0"
               max="10.0"
-            />
-    
-            <input type="submit" value="SUBMIT REVIEW" className="submitBtn" />
+              style={{width:'12%'}}
+            /> */}
+
+            {/* <div className="input-prepend">
+              <span className="add-on">0</span>
+              <input
+                type="range"
+                id="rating"
+                name="rating"
+                min="0"
+                max="10"
+              />
+              <span className="add-on">10</span>
+            </div> */}
+{/* 
+            <input type="range" id="numberSlider" name="number" min="0.1" max="10" step="0.25"/>
+            <output for="numberSlider" id="sliderValue">50</output> */}
+            <SliderComponent></SliderComponent>
+          
+  
+            <button type="submit" className="btn btn-success">
+              Submit review
+            </button>
+
           </form>
         )}
       </div>
