736155c0c2c466a774f8751c95c07aec7761aee7
Joonatan N <Joona-ni@outlook.com>
2023-12-04 16:07:47 +0200

    Ryhmän jäsenen voi admin poistaa

--- server/models/users_groups_model.js
+++ server/models/users_groups_model.js
@@ -4,7 +4,7 @@ const sql = {
     INSERT_USER_GROUP: 'INSERT INTO users_groups (username, group_name, admin) VALUES ($1, $2, $3)',
     GET_USER_GROUP: 'SELECT username, group_name, admin FROM users_groups',
     UPDATE_USERS_GROUP: 'UPDATE users_groups SET username = $1, group_name = $2, admin = $3 WHERE username = $1',
-    DELETE_USERS_GROUPS: 'DELETE FROM users_groups WHERE username = $1',
+    DELETE_USERS_GROUPS: 'DELETE FROM users_groups WHERE username = $2 AND group_name = $1',
     GET_GROUPMEMBERS: 'SELECT username, admin FROM users_groups WHERE group_name = $1',
     GET_REVIEWSFORGROUP: 'SELECT ug.username, ug.group_name, r.review, r.moviedb_movieid FROM users_groups ug JOIN reviews r ON ug.username = r.username WHERE ug.group_name = $1',
 };
@@ -27,10 +27,10 @@ async function updateUserGroup(username,group_name,admin){
     await pgPool.query(sql.UPDATE_USERS_GROUP, [username,group_name,admin]);
     console.log(sql.UPDATE_USERS_GROUP, [username,group_name,admin]);
 }
-async function deleteUserGroup(username){
-    await pgPool.query(sql.DELETE_USERS_GROUPS, [username]);
-    console.log(sql.DELETE_USERS_GROUPS, [username]);
-}
+async function deleteUserGroup(group_name,username) {
+    await pgPool.query(sql.DELETE_USERS_GROUPS, [group_name,username]);
+    console.log(sql.DELETE_USERS_GROUPS, [group_name,username]);
+  }
 async function getReviewsForGroup(group_name){
     const result = await pgPool.query(sql.GET_REVIEWSFORGROUP, [group_name]);
     const rows = result.rows;
