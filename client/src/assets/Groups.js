3f8820c3ed1b50d8dc2dc84e3a7480efbb0f2b93
Joonatan N <Joona-ni@outlook.com>
2023-12-04 13:23:05 +0200

    Group ja groupdetailia korjailtu

--- client/src/assets/Groups.js
+++ client/src/assets/Groups.js
@@ -4,111 +4,133 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { GroupModal } from '../components/Groupcreate.js';
 import { Link } from 'react-router-dom';
-
+import './Group.css';
 
 function Groups() {
-    const [groups, setGroups] = useState([]);
-    const [mygroups, setMyGroups] = useState([]);
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [groups, setGroups] = useState([]);
+  const [mygroups, setMyGroups] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    const username = sessionStorage.getItem('username');
-    // Function to fetch all groups
-    const fetchGroups = async () => {
-        try {
-            const response = await axios.get('http://localhost:3001/groups'); //
-            setGroups(response.data);
-        } catch (error) {
-            console.error('Error fetching groups:', error.response ? error.response.data : error.message);
-        }
-        try {
-            const response = await axios.get(`http://localhost:3001/groups/user/${username}`);
-            setMyGroups(response.data);
-        }
-        catch (error) {
-            console.error('Error fetching My Groups:', error.response ? error.response.data : error.message);
-        }
-    };
+  const username = sessionStorage.getItem('username');
+  // Function to fetch all groups
+  const fetchGroups = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/groups'); //
+      setGroups(response.data);
+    } catch (error) {
+      console.error('Error fetching groups:', error.response ? error.response.data : error.message);
+    }
+    try {
+      const response = await axios.get(`http://localhost:3001/groups/user/${username}`);
+      setMyGroups(response.data);
+    }
+    catch (error) {
+      console.error('Error fetching My Groups:', error.response ? error.response.data : error.message);
+    }
+  };
 
-    useEffect(() => {
-        // Fetch groups when the component mounts
-        fetchGroups();
-    }, []);
+  useEffect(() => {
+    // Fetch groups when the component mounts
+    fetchGroups();
+  }, []);
 
-    const handleOpenModal = () => {
-        setModalIsOpen(true);
-    };
+  const handleOpenModal = () => {
+    setModalIsOpen(true);
+  };
 
-    const handleCloseModal = () => {
-        setModalIsOpen(false);
-    };
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
 
-    let admin = true;
+  let admin = true;
 
-    const handleModalSubmit = async ({ group_name, group_description}) => {
-      try {
-            const groupResponse = await axios.post('http://localhost:3001/groups', {
-              group_name,
-              group_description,
-            });
-            await axios.post('http://localhost:3001/users_groups', {
-              username,
-              group_name,
-              admin,
-            });
-      }
-      catch (error){
-          console.error('Error during group creation:', error.response ? error.response.data : error.message);
-      }
-  
-      // Close the modal
-      setModalIsOpen(false);
-    };
+  const handleModalSubmit = async ({ group_name, group_description }) => {
+    try {
+      const groupResponse = await axios.post('http://localhost:3001/groups', {
+        group_name,
+        group_description,
+      });
+      await axios.post('http://localhost:3001/users_groups', {
+        username,
+        group_name,
+        admin,
+      });
+    }
+    catch (error) {
+      console.error('Error during group creation:', error.response ? error.response.data : error.message);
+    }
 
-return (
+    // Close the modal
+    setModalIsOpen(false);
+  };
+
+  return (
     <div>
-    <Container>
-      <h1>All Groups</h1>
-      <button onClick={handleOpenModal}>Add Group</button>
-          <GroupModal
-            isOpen={modalIsOpen}
-            onRequestClose={handleCloseModal}
-            onSubmit={handleModalSubmit}
-          />
-      <Row>
-        <Col md={6}>
-        <div
-            className="group-container"
-            style={{ height: '400px', overflowY: 'auto', border: '1px solid #ddd', padding: '10px' }}
-          >
-            {groups.map((group) => (
-              <div key={group.group_name} className="mb-3">
-                <div className="group-box p-3 border">
-                  <h3>{group.group_name}</h3>
-                  <p style={{ maxHeight: '80px', overflow: 'hidden' }}>{group.group_description}</p>
-                  {/* Add more details or actions as needed */}
+      <Container>
+        <GroupModal
+          isOpen={modalIsOpen}
+          onRequestClose={handleCloseModal}
+          onSubmit={handleModalSubmit}
+        />
+        <Row>
+          <Col md={6}>
+          <h1>All Groups</h1>
+            <div
+              className="group-container"
+              style={{ height: '400px', overflowY: 'auto', border: '5px solid #ddd', padding: '10px' }}
+            >
+              {groups.map((group) => (
+                <div key={group.group_name} className="mb-3">
+                  <div className="group-box p-3 border" style={{ backgroundColor: 'lightblue' }}>
+                    <h3>{group.group_name}</h3>
+                    <p style={{ maxHeight: '80px', overflow: 'hidden' }}>{group.group_description}</p>
+                    {/* Add more details or actions as needed */}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </Col>
-        <Col md={6}>
-          {/* My Groups */}
-          <div className="group-container" style={{ height: '400px', overflowY: 'auto', border: '1px solid #ddd', padding: '10px' }}>
-            {mygroups.map((group) => (
-         <Link to={`/groups/${group.group_name}`} key={group.group_name} className="mb-3" style={{ textDecoration: 'none', color: 'black' }}>
-            <div className="group-box p-3 border">
-              <h3>{group.group_name}</h3>
-              <p style={{ maxHeight: '80px', overflow: 'hidden' }}>{group.group_description}</p>
-              {/* Add more details or actions as needed */}
+              ))}
             </div>
-              </Link>
-            ))}
-          </div>
+          </Col>
+          <Col md={6}>
+          <h1>Your Groups</h1>
+            {/* My Groups */}
+            <div
+              className="group-container"
+              style={{ height: '400px', overflowY: 'auto', border: '5px solid #ddd', padding: '10px' }}
+            >
+              {mygroups.map((group, index) => (
+                <Link
+                  to={`/groups/${group.group_name}`}
+                  key={group.group_name}
+                  className={`mb-3 ${index !== mygroups.length - 1 ? 'mb-5' : ''}`}  // Add additional margin for all but the last box
+                  style={{ textDecoration: 'none', color: 'black', display: 'block' }}
+                >
+                  <div className="group-box p-3 border" style={{ backgroundColor: 'lightblue' }}>
+                    <h3>{group.group_name}</h3>
+                    <p style={{ maxHeight: '80px', overflow: 'hidden' }}>{group.group_description}</p>
+                    {/* Add more details or actions as needed */}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </Col>
+        </Row>
+        <Row className="mt-4"> {/* Add top margin and a new row for the button */}
+        <Col md={12}>
+          <button
+            onClick={handleOpenModal}
+            className="btn btn-primary"
+            style={{
+              backgroundColor: 'green',
+              borderColor: 'green',
+            }}
+          >
+            Add group
+          </button>
         </Col>
       </Row>
-    </Container>
-  </div>
-    );
+      </Container>
+    </div>
+  );
 }
 
 export default Groups;
--- client/src/components/GroupDetail.js
+++ client/src/components/GroupDetail.js
@@ -2,69 +2,85 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Crown from './images/crown.png';
+import './GroupDetail.css';
+import FinnkinoFetch from './FinnkinoFetch';
 
 function GroupDetail() {
-    const { group_name } = useParams();
-    const [usernames, setUsernames] = useState([]);
-    const [reviews, setReviews] = useState([]);
-    const [movies, setMovies] = useState([]);
-  
-    const fetchGroupMembers = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3001/users_groups/${group_name}`);
-        setUsernames(response.data.map(user => ({ username: user.username, admin: user.admin })));
-      } catch (error) {
-        console.error('Error fetching group members:', error.response ? error.response.data : error.message);
-      }
-    };
-  
-    const fetchGroupReviews = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3001/users_groups/reviewforgroup/${group_name}`);
-        setReviews(response.data);
-      } catch (error) {
-        console.error('Error fetching group reviews:', error.response ? error.response.data : error.message);
-      }
-    };
-  
-    const fetchMoviesForReviews = async () => {
-      try {
-        if (reviews.length > 0) {
-          const movieIds = reviews.map((review) => review.moviedb_movieid);
-  
-          const responses = await Promise.all(
-            movieIds.map(async (movieId) => {
-              try {
-                const response = await axios.get(`http://localhost:3001/movies/id/${movieId}`);
-                return response.data;
-              } catch (error) {
-                console.error(`Error fetching movie with ID ${movieId}:`, error.response ? error.response.data : error.message);
-                return null;
-              }
-            })
-          );
-  
-          const movieDatas = responses.filter((response) => response !== null);
-          setMovies(movieDatas);
-        }
-      } catch (error) {
-        console.error('Error fetching movie data:', error);
-      }
-    };
-    
-    useEffect(() => {
-      fetchGroupMembers();
-      fetchGroupReviews();
-    }, []);
-  
-    useEffect(() => {
-      fetchMoviesForReviews();
-    }, [reviews]);
+  const { group_name } = useParams();
+  const [usernames, setUsernames] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [isNewsToggled, setIsNewsToggled] = useState(true);
 
-    return (
+  const fetchGroupMembers = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/users_groups/${group_name}`);
+      setUsernames(response.data.map(user => ({ username: user.username, admin: user.admin })));
+    } catch (error) {
+      console.error('Error fetching group members:', error.response ? error.response.data : error.message);
+    }
+  };
+
+  const fetchGroupReviews = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/users_groups/reviewforgroup/${group_name}`);
+      setReviews(response.data);
+    } catch (error) {
+      console.error('Error fetching group reviews:', error.response ? error.response.data : error.message);
+    }
+  };
+
+  const fetchMoviesForReviews = async () => {
+    try {
+      if (reviews.length > 0) {
+        const movieIds = reviews.map((review) => review.moviedb_movieid);
+
+        const responses = await Promise.all(
+          movieIds.map(async (movieId) => {
+            try {
+              const response = await axios.get(`http://localhost:3001/movies/id/${movieId}`);
+              return response.data;
+            } catch (error) {
+              console.error(`Error fetching movie with ID ${movieId}:`, error.response ? error.response.data : error.message);
+              return null;
+            }
+          })
+        );
+
+        const movieDatas = responses.filter((response) => response !== null);
+        setMovies(movieDatas);
+      }
+    } catch (error) {
+      console.error('Error fetching movie data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGroupMembers();
+    fetchGroupReviews();
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesForReviews();
+  }, [reviews]);
+
+  const handleToggle = () => {
+    setIsNewsToggled(!isNewsToggled);
+  };
+
+  const toggleButton = (
+    <button onClick={handleToggle}>
+      {isNewsToggled ? 'Hide News' : 'Show News'}
+    </button>
+  );
+
+  const newsComponent = isNewsToggled && <FinnkinoFetch />;
+
+  return (
+      <div>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           {/* Display group reviews on the left side */}
-          <div style={{ width: '45%', border: '1px solid #ccc', padding: '10px', marginTop: '10px' }}>
+          <div style={{ width: '45%', border: '1px solid #ccc', padding: '10px', marginTop: '10px', border: '5px solid #ddd' }}>
             <h3>Group Reviews</h3>
             <ul>
               {reviews.map((review, index) => (
@@ -74,21 +90,37 @@ function GroupDetail() {
               ))}
             </ul>
           </div>
-      
+
           {/* Display group members on the right side */}
-          <div style={{ width: '45%', border: '1px solid #ccc', padding: '10px', marginTop: '10px' }}>
+          <div style={{ width: '45%', border: '1px solid #ccc', padding: '10px', marginTop: '10px', border: '5px solid #ddd' }}>
             <h3>Group Members</h3>
             <ul>
               {usernames.map((user, index) => (
-            <li key={index}>
-                <strong>{user.username}</strong> - {user.admin ? 'Admin' : 'Member'}
-                {user.admin && <img src={ Crown } alt="Crown" style={{ width: '25px', height: '25px', marginLeft: '2px', marginTop: '-6px'}} />}
-            </li>
+                <li key={index}>
+                  <strong>{user.username}</strong> - {user.admin ? 'Admin' : 'Member'}
+                  {user.admin && <img src={Crown} alt="Crown" style={{ width: '25px', height: '25px', marginLeft: '2px', marginTop: '-6px' }} />}
+                </li>
               ))}
             </ul>
           </div>
         </div>
-      );
-}      
-  export default GroupDetail;
-  
+    <div className='news-container' style={{
+      display: 'flex',
+      width: '45%',
+      height: '400px',
+      overflowY: 'scroll',
+      float: 'left',
+      border: '5px solid #ddd',
+      marginTop: '5px',
+      backgroundColor: 'white',
+      borderRadius: '10px',
+      marginBottom: '50px'
+    }}>
+      {toggleButton}
+      {newsComponent}
+    </div>
+    </div>
+  );
+}
+
+export default GroupDetail;
