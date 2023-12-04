b1a02c8af07684c8d9a1d24573b111fb6b9cd83e
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-12-04 15:37:11 +0200

    landing page customizations finnkino news style

--- client/src/components/FinnkinoFetch.js
+++ client/src/components/FinnkinoFetch.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { parseString } from 'xml2js';
+import { Card } from 'react-bootstrap';
 
 const FinnkinoFetch = () => {
   const url = 'https://www.finnkino.fi/xml/News/';
@@ -36,10 +37,22 @@ const FinnkinoFetch = () => {
 
           {news.map((article, index) => (
             article.Title.includes("Leffauutiset") && (
-              <div key={index}>
-                <a href={article.ArticleURL} style={{color:'black'}}><h4>{article.Title.slice(13)}</h4></a>
-                <p>{article.HTMLLead}</p>
-              </div>
+                <>
+              {article.HTMLLead.length > 0 && (
+                  
+                  <Card key={index} id={index}>
+                  <Card.Title style={{margin:'auto auto '}}>
+                  <a href={article.ArticleURL} style={{color:'black'}}><h4 style={{fontWeight:'700'}}>{article.Title.slice(13)}</h4></a>
+                  </Card.Title>
+
+                  <Card.Footer>
+                  <p>{article.HTMLLead}</p>
+                  </Card.Footer>
+              </Card>
+
+              )}
+              </>
+
             )
           ))}
         </div>
