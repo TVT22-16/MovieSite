4c6698ac6b7a33b2f8e28208886031b89819314c
Eerik <112471297+Eerik-Vaisanen@users.noreply.github.com>
2023-12-07 22:20:34 +0200

    Watchlist logic updated
    User can't add same movie to watchlist twice and the movies are now mapped in Profile page. By pressing the movie poster, it takes you to the movies info page.

--- server/models/watchlist_model.js
+++ server/models/watchlist_model.js
@@ -3,19 +3,21 @@ const pgPool = require('../postgre/connection');
 const sql = {
 ADD_TO_WATCHLIST: `INSERT INTO watchlist (username, moviedb_movieid, title, overview, release_date, poster_path, added_at) VALUES ($1, $2, $3, $4, $5, $6, NOW()) RETURNING *;`,
 GET_WATCHLIST_MOVIES: 'SELECT * FROM watchlist WHERE username = $1',
+CHECK_IF_MOVIE_IN_WATCHLIST: 'SELECT EXISTS (SELECT 1 FROM watchlist WHERE username = $1 AND moviedb_movieid = $2)',
 
 
 }
 
 async function addToWatchlist(username, moviedb_movieid, title, overview, release_date, poster_path) {
-    try {
-      const result = await pgPool.query(sql.ADD_TO_WATCHLIST, [username, moviedb_movieid, title, overview, release_date, poster_path]);
-      return result;
-    } catch (error) {
-      console.error('Error adding to watchlist:', error);
-      throw error;
-    }
+  try {
+    const result = await pgPool.query(sql.ADD_TO_WATCHLIST, [username, moviedb_movieid, title, overview, release_date, poster_path]);
+    return result;
+  } catch (error) {
+    console.error('Error adding to watchlist:', error);
+    throw error;
   }
+}
+
   
  async function getWatchlistMovies(username) {
     try {
@@ -27,5 +29,15 @@ async function addToWatchlist(username, moviedb_movieid, title, overview, releas
     }
   }
 
+  async function checkIfMovieInWatchlist(username, moviedb_movieid) {
+    try {
+      const result = await pgPool.query(sql.CHECK_IF_MOVIE_IN_WATCHLIST, [username, moviedb_movieid]);
+      return result.rows[0].exists;
+    } catch (error) {
+      console.error('Error checking if movie is in watchlist:', error);
+      throw error;
+    }
+  }
 
-module.exports = {addToWatchlist, getWatchlistMovies}
+
+module.exports = {addToWatchlist, getWatchlistMovies, checkIfMovieInWatchlist}
