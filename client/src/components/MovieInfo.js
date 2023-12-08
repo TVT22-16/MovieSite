8d32b343f33d6b050827463c2af1afdb6166c900
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2023-12-08 12:48:12 +0200

    bottommargin movieinfo

--- client/src/components/MovieInfo.js
+++ client/src/components/MovieInfo.js
@@ -145,10 +145,10 @@ const MovieInfo = () => {
  
         </div>
         <ReviewForm moviedb_movieid={params}/>
-          <div style={{display: 'flex',flexDirection:'column',width:'80%', gap:'20px', alignItems:'center'}}>
+          <div style={{display: 'flex', marginBottom:'2%',flexDirection:'column',width:'80%', gap:'20px', alignItems:'center'}}>
             <Reviews movieid={params}> </Reviews>
             
-          </div>
+        </div>
 
 
 
