6b2098f05e3359627eba17b07f39555f62fc88c3
unknown <joonasroska123@gmail.com>
2023-12-15 14:58:55 +0200

    review body bug fix

--- client/src/components/DeleteReview.js
+++ client/src/components/DeleteReview.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import baseUrl from './baseUrl';
 
-const DeleteReview = (id) => {
-  axios.delete(`${baseUrl}/reviews/delete/${id}`)
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.error('Error deleting review:', error);
-    });
-
-  return null;
-}
+const DeleteReview = async (id) => {
+  try {
+    const response = await axios.delete(`${baseUrl}/reviews/delete/${id}`);
+    console.log(`DELETE REVIEW!½!!!!!: ${baseUrl}/reviews/delete/${id}`)
+    console.log(response.data);
+    // You can return the response data or handle it as needed
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting review:', error);
+    throw error; // Re-throw the error to be caught by the calling function
+  }
+};
 
 export default DeleteReview;
